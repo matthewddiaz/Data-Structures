0ed4347f32ba4154e822e37def720417e6a3c9bf
Matthew D Diaz <matthewdiaz10@ufl.edu>
2017-03-09 19:22:30 -0500

    Added toArray() and createIteartor() to Queue class. Also created QueueTest to test class Queue. TestData class provides a reusable data set to test

--- src/com/matthewddiaz/datastructures/queue/Queue.java
+++ src/com/matthewddiaz/datastructures/queue/Queue.java
@@ -1,11 +1,21 @@
 package com.matthewddiaz.datastructures.queue;
 
+import com.matthewddiaz.designpatterns.behavioralPatterns.Iterator;
+
 /**
  * Created by matthewdiaz on 8/29/16.
  */
 public class Queue<T> {
     private Node<T> head;
     private Node<T> tail;
+    //necessary for toArray() method; need to know the length of the array.
+    private int numOfElements;
+
+    public Queue(){
+        head = null;
+        tail = head;
+        numOfElements = 0;
+    }
 
     private class Node<T>{
         T element;
@@ -32,6 +42,7 @@ public class Queue<T> {
             tail.next = createNewNode(element);
             tail = tail.next;
         }
+        numOfElements++;
     }
 
     /**
@@ -45,6 +56,7 @@ public class Queue<T> {
 
         T value = head.element;
         head = head.next;
+        numOfElements--;
         return value;
     }
 
@@ -58,5 +70,53 @@ public class Queue<T> {
         node.element = element;
         node.next = null;
         return node;
+    }
+
+    /**
+     * Creates a new Iterator. The iterator traverses the list from the
+     * head to the last element the tail in the queue.
+     * @return
+     */
+    public Iterator<T> createIterator(){
+        return new Iterator<T>() {
+            private Node<T> currentPosition = null;
+
+            @Override
+            public void first() {
+                this.currentPosition = head;
+            }
+
+            @Override
+            public T currentElement() {
+                return currentPosition.element;
+            }
+
+            @Override
+            public void next() {
+                currentPosition = currentPosition.next;
+            }
+
+            @Override
+            public boolean isDone() {
+                return currentPosition == null;
+            }
+        };
+    }
+
+    /**
+     * Returns a new Object array of of the elements in the Queue
+     * the array contains the elements ordered from head to tail
+     * of the Queue.
+     * @return
+     */
+    public Object[] toArray(){
+        Object[] array = new Object[numOfElements];
+        Iterator<T> iterator = createIterator();
+
+        int currentPosition = 0;
+        for(iterator.first(); !iterator.isDone(); iterator.next()){
+            array[currentPosition++] = iterator.currentElement();
+        }
+        return array;
     }
 }
--- test/com/matthewddiaz/datastructures/queue/QueueTest.java
+++ test/com/matthewddiaz/datastructures/queue/QueueTest.java
@@ -1,8 +1,59 @@
+package com.matthewddiaz.datastructures.queue;
+
+import data.TestData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by matthewdiaz on 3/9/17.
  */
 class QueueTest {
+    private Queue<String> queueOfStudents;
+    private Queue<Double> queueOfGrades;
+
+    private String[] students;
+    private Double[] grades;
+
+    @BeforeEach
+    void setUp() {
+        queueOfStudents = new Queue<>();
+        queueOfGrades = new Queue<>();
+        students = TestData.getStringArray();
+        grades = TestData.getDoubleArray();
+    }
+
+    @Test
+    void testingAnEmptyQueue() {
+        boolean expectedResult = true;
+        boolean actualResult = queueOfGrades.isEmpty();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testingANonEmptyQueue() {
+
+    }
+
+    @Test
+    void testingEnqueueForString() {
+
+    }
+
+    @Test
+    void testingDequeueForString() {
+
+    }
+
+    @Test
+    void testingEnqueueForDouble() {
+
+    }
+
+    @Test
+    void testingDequeueForDouble() {
+
+    }
 
 }
