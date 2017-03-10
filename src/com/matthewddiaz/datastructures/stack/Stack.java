ccac89c7f579daaa663675bcbb524571950b3eed
Matthew D Diaz <matthewdiaz10@ufl.edu>
2017-03-09 23:21:58 -0500

    Add toArray() and createIterator() methods ot Stack.java and also created StackTest.java to test Stack

--- src/com/matthewddiaz/datastructures/stack/Stack.java
+++ src/com/matthewddiaz/datastructures/stack/Stack.java
@@ -1,14 +1,23 @@
 package com.matthewddiaz.datastructures.stack;
 
+import com.matthewddiaz.designpatterns.behavioralPatterns.Iterator;
+
 /**
  * Created by matthewdiaz on 8/28/16.
  */
 public class Stack<T> {
     private Node<T> head;
+    //necessary for toArray() method; need to know the length of the array.
+    private int numOfElements;
 
     private class Node<T>{
         T element;
         Node next;
+    }
+
+    public Stack(){
+        head = null;
+        numOfElements = 0;
     }
 
     /**
@@ -23,6 +32,7 @@ public class Stack<T> {
             node.next = head;
             head = node;
         }
+        numOfElements++;
     }
 
     /**
@@ -36,6 +46,7 @@ public class Stack<T> {
 
         T val = head.element;
         head = head.next;
+        numOfElements--;
         return val;
     }
 
@@ -58,6 +69,52 @@ public class Stack<T> {
      */
     public boolean isEmpty(){
         return head == null;
+    }
+
+    /**
+     * Returns a new Iterator of the Stack
+     * @return
+     */
+    public Iterator<T> createIterator(){
+        return new Iterator<T>() {
+            private Node<T> currentPosition;
+
+            @Override
+            public void first() {
+                currentPosition = head;
+            }
+
+            @Override
+            public T currentElement() {
+                return currentPosition.element;
+            }
+
+            @Override
+            public void next() {
+                currentPosition = currentPosition.next;
+            }
+
+            @Override
+            public boolean isDone() {
+                return currentPosition == null;
+            }
+        };
+    }
+
+    /**
+     * Returns a new array of the elements in the stack ordered from the element at the
+     * top of the stack to the bottom.
+     * @return
+     */
+    public Object[] toArray(){
+        Object[] array = new Object[this.numOfElements];
+        Iterator<T> iterator = createIterator();
+        int currentPosition = 0;
+
+        for(iterator.first(); iterator.isDone(); iterator.next()){
+            array[currentPosition++] = iterator.currentElement();
+        }
+        return array;
     }
 
     /**
---
+++ test/com/matthewddiaz/datastructures/stack/StackTest.java
@@ -0,0 +1,37 @@
+package com.matthewddiaz.datastructures.stack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by matthewdiaz on 3/9/17.
+ */
+class StackTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void push() {
+
+    }
+
+    @Test
+    void pop() {
+
+    }
+
+    @Test
+    void peak() {
+
+    }
+
+    @Test
+    void isEmpty() {
+
+    }
+
+}
