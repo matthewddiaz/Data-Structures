c76b080292c2d1f4b21e6e67ef4a80f50d6ae1cc
Matthew D Diaz <matthewdiaz10@ufl.edu>
2017-03-31 18:17:49 -0400

    Updated MaxHeap to work on arrays that are also only partially full

--- src/com/matthewddiaz/datastructures/heap/MaxHeap.java
+++ src/com/matthewddiaz/datastructures/heap/MaxHeap.java
@@ -12,8 +12,19 @@ package com.matthewddiaz.datastructures.heap;
  * NOTE: Max Heap is extremely useful; since it is used to implement HeapSort and a Max Priority Queue.
  */
 public class MaxHeap {
-    //heapSize can range from [0, array.length]
+    //heapSize is equal equal to # of elements in array. Can range from [0, array.length]
     private int heapSize;
+    //heap length is equal to array.length
+    private int length;
+
+    /**
+     * NOTE: It's important to call any of the two buildMaxHeap to turn the input array into
+     * an actual Max Heap. The constructor does not convert do this.
+     */
+    public MaxHeap(){
+        this.heapSize = 0;
+        this.length = 0;
+    }
 
     /**
      * maxHeapify ensures that Max Heap rule is enforced. If the rule is broken for the
@@ -21,7 +32,7 @@ public class MaxHeap {
      * child becomes the parent and the parent gets filled into the larger child position. Then
      * maxHeapify is called again on the larger child position to check the rule again.
      *
-     * NOTE: Max Heap rule is that the value of parentIndex is greater than
+     * NOTE1: Max Heap rule is that the value of parentIndex is greater than
      * or equal to the value of its children.
      *
      * Running Time: θ(lg(n))
@@ -29,6 +40,13 @@ public class MaxHeap {
      * @param parentIndex
      */
     public void maxHeapify(Comparable[] array, int parentIndex){
+        //returns immediately if parentIndex is larger than the index of the last nonleaf node in the heap;
+        //because the element at parentIndex is actually a leaf element.
+        int lastNonLeafElementIndex = (this.heapSize/2) - 1;
+        if(parentIndex > lastNonLeafElementIndex){
+            return;
+        }
+
         //NOTE: To have A[0] as the starting node instead of A[1]
         //leftChildIndex & rightChildIndex is offset by an addition 1
         int leftChildIndex = 2*parentIndex + 1;
@@ -36,12 +54,12 @@ public class MaxHeap {
         int largestElementIndex = parentIndex;
 
         //check if leftChildIndex is less than heapSize & leftChild is greater than parent
-        if((leftChildIndex <= this.heapSize) && (array[leftChildIndex].compareTo(array[parentIndex]) > 0)){
+        if((leftChildIndex < this.heapSize) && (array[leftChildIndex].compareTo(array[parentIndex]) > 0)){
             largestElementIndex = leftChildIndex;
         }
 
         //check if rightChildIndex is less than heapSize & rightChild is greater than largestElement
-        if((rightChildIndex <= this.heapSize) && (array[rightChildIndex].compareTo(array[largestElementIndex]) > 0)){
+        if((rightChildIndex < this.heapSize) && (array[rightChildIndex].compareTo(array[largestElementIndex]) > 0)){
             largestElementIndex = rightChildIndex;
         }
 
@@ -55,35 +73,43 @@ public class MaxHeap {
     }
 
     /**
-     * Builds input array for an array of size n into a max heap with a bottom up approach by calling maxHeapify on
-     * every index that may have a child. That is from [1.. (n/2)]
-     *
-     * NOTE: n/2 is the last nonleaf element. Elements from [(n/2) + 1, n] are all leaf elements
-     * and can't have children.
-     *
-     * NOTE: heapSize is set to (array.length - 1)
+     * This method calls buildMaxHeap(array, numOfElements) and assumes that
+     * the number of elements in the array is equal to array.length; that the array is full.
      *
      * Running Time: θ(n)
      * @param array
      */
-    public void buildMaxHeap(Comparable[] array){
-        this.heapSize = array.length - 1;
-        for(int nonLeafIndex = this.heapSize/2; nonLeafIndex >= 0; nonLeafIndex--){
-            maxHeapify(array, nonLeafIndex);
-        }
+    public void buildMaxHeap(Comparable[] array) throws Exception {
+        buildMaxHeap(array, array.length);
      }
 
     /**
-     * Same as buildMaxHeap() with one parameter. However heapSize is explicitly input
-     * by the user.
+     * Builds input array for an array of size n into a max heap with a bottom up approach by calling maxHeapify on
+     * every index that may have a child. That is from [((n/2) - 1)... 0]
+     * NOTE: n is the number of elements in the heap/array
+     *
+     * Number of elements is explicitly input by the user.
      * @param array
-     * @param heapSize
+     * @param numOfElements range [0...array.length]
      *
      * Running Time: θ(n)
      */
-    public void buildMaxHeap(Comparable[] array, int heapSize){
-        this.heapSize = heapSize;
-        for(int nonLeafIndex = this.heapSize/2; nonLeafIndex >= 0; nonLeafIndex--){
+    public void buildMaxHeap(Comparable[] array, int numOfElements) throws Exception {
+        if(numOfElements < 0 || numOfElements > array.length){
+            throw new Exception("Number of elements for this heap is out of bound. " +
+                    "Acceptable range for numOfElements is [0...array.length]");
+        }
+
+        this.length = array.length;
+        this.heapSize = numOfElements;
+        //don't need to call maxHeapify if the heap is empty or only has 1 element
+        if(this.heapSize < 2){
+            return;
+        }
+
+        //index of last nonLeaf element in array
+        int nonLeafIndex = (this.heapSize/2) - 1;
+        for(; nonLeafIndex >= 0; nonLeafIndex--){
             maxHeapify(array, nonLeafIndex);
         }
     }
@@ -94,15 +120,43 @@ public class MaxHeap {
         array[rightElement] = tempElement;
     }
 
+    /**
+     * Returns true when heap is empty (contains 0 elements)
+     * @return
+     */
+    public boolean isEmpty(){
+        return this.heapSize == 0;
+    }
+
+    /**
+     * Returns true when heap is full (heapSize is equal to the length)
+     * @return
+     */
+    public boolean isFull(){
+        return this.heapSize == this.length;
+    }
+
     public void decrementHeapSize(){
         this.heapSize--;
     }
 
     public void incrementHeapSize(){ this.heapSize++;}
 
+    /**
+     * The number of elements currently in the heap.
+     * Ranges from [0... A.length]
+     * @return
+     */
     public int getHeapSize(){
         return this.heapSize;
     }
+
+    /**
+     * The length of the input array.
+     * This is equal to A.length
+     * @return
+     */
+    public int getLength() { return this.length; }
 
     public void setHeapSize(int heapSize){
         this.heapSize = heapSize;
--- test/com/matthewddiaz/datastructures/heap/MaxHeapTest.java
+++ test/com/matthewddiaz/datastructures/heap/MaxHeapTest.java
@@ -29,7 +29,11 @@ class MaxHeapTest {
     @Test
     void buildIntegerMaxHeap() {
         Integer[] expectedArray = {180, 103, 105, 16, 20, 100, 15, 1, 11, 4, 17, 19};
-        maxHeap.buildMaxHeap(ages);
+        try {
+            maxHeap.buildMaxHeap(ages);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         assertArrayEquals(expectedArray, ages);
     }
 
@@ -39,7 +43,11 @@ class MaxHeapTest {
     @Test
     void buildStringMaxHeap() {
         String[] expectedArray = {"Vegeta", "Master Roshi", "Piccolo", "Krillian", "Goku", "Gohan", "Bulma", "Cell"};
-        maxHeap.buildMaxHeap(students);
+        try {
+            maxHeap.buildMaxHeap(students);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         assertArrayEquals(expectedArray, students);
     }
 
@@ -49,7 +57,63 @@ class MaxHeapTest {
     @Test
     void buildDoubleMaxHeap() {
         Double[] expectedArray = {105.4, 103.2, 17.4, 100.4, 19.3, 16.6, 1.1, 18.18, 16.5, 15.1, 11.1, 4.3, 2.3, -180.2};
-        maxHeap.buildMaxHeap(grades);
+        try {
+            maxHeap.buildMaxHeap(grades);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         assertArrayEquals(expectedArray, grades);
+    }
+
+    @Test
+    void testBuildMaxHeapWithNotFullEvenElementsArray() {
+        Double[] expectedResult = {103.2, 50.1, 23.6, 10.4, 19.3, 1.4, null, null, null, null};
+        Double[] partialClassGrades = new Double[10];
+        partialClassGrades[0] = 10.4;
+        partialClassGrades[1] = 103.2;
+        partialClassGrades[2] = 1.4;
+        partialClassGrades[3] = 50.1;
+        partialClassGrades[4] = 19.3;
+        partialClassGrades[5] = 23.6;
+
+        try {
+            maxHeap.buildMaxHeap(partialClassGrades, 6);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertArrayEquals(expectedResult, partialClassGrades);
+    }
+
+    @Test
+    void testBuildMaxHeapWithNotFullOddElementsArray() {
+        Integer[] expectedResult = {30, 21, 11, 15, 19, null, null, null, null, null};
+
+        Integer[] studentAges = new Integer[10];
+        studentAges[0] = 15;
+        studentAges[1] = 21;
+        studentAges[2] = 11;
+        studentAges[3] = 30;
+        studentAges[4] = 19;
+
+        try {
+            maxHeap.buildMaxHeap(studentAges, 5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertArrayEquals(expectedResult, studentAges);
+    }
+
+    /**
+     * Test isFull() method
+     */
+    @Test
+    void buildMaxHeapIsFull(){
+        try {
+            maxHeap.buildMaxHeap(students);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        boolean actualResult = maxHeap.isFull();
+        assertTrue(actualResult);
     }
 }
