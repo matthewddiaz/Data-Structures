3229b62b441366ae60aafd0df8cf61f841a8d67f
Matthew D Diaz <matthewdiaz10@ufl.edu>
2017-07-21 12:58:09 -0400

    Add comments to directed and undirected graph subclasses

--- src/com/matthewddiaz/datastructures/graphs/DirectedGraph.java
+++ src/com/matthewddiaz/datastructures/graphs/DirectedGraph.java
@@ -9,25 +9,54 @@ import java.util.Set;
  */
 public class DirectedGraph extends Graph {
 
+    /**
+     * Constructor for Directed graph
+     * @param vertexSet set of vertices of the graph
+     * @param edgeSet set of edges of the graph
+     */
     public DirectedGraph(Set<Vertex> vertexSet, Set<Edge> edgeSet){
         super(vertexSet, edgeSet);
     }
 
+    /**
+     *
+     * @param source the first vertex in the pair
+     * @param destination the second vertex in the pair
+     * @return always returns true. Self loops are also allowed
+     */
     @Override
     boolean isEdgeValid(Vertex source, Vertex destination) {
         return true;
     }
 
+    /**
+     * Just adds adjacent vertex to source's adjacency list
+     * @param adjArray adjacency array
+     * @param source source vertex
+     * @param adjacentVertex vertex that is adjacent to source vertex
+     */
     @Override
     void addAdjacentVertexToAdjArr(List<Vertex>[] adjArray, Vertex source, Vertex adjacentVertex) {
         addAdjacentVertex(adjArray, source, adjacentVertex);
     }
 
+    /**
+     * Just adds adjacent vertex to source's adjacency list
+     * @param adjList adjacency array (map)
+     * @param source  source vertex
+     * @param adjacentVertex vertex that is adjacent to source vertex
+     */
     @Override
     void addAdjacentVertexToAdjMap(Map<Integer, List<Vertex>> adjList, Vertex source, Vertex adjacentVertex) {
         addAdjacentVertex(adjList, source, adjacentVertex);
     }
 
+    /**
+     * Just adds adjacent vertex to source's adjacency list
+     * @param adjMatrix adjacent matrix 2D array
+     * @param source source vertex
+     * @param adjVertex vertex that is adjacent to source vertex
+     */
     @Override
     void addAdjVertexToAdjMatrix(int[][] adjMatrix, Vertex source, Vertex adjVertex) {
         addVertexToAdjMatrix(adjMatrix, source.id, adjVertex.id);
--- src/com/matthewddiaz/datastructures/graphs/UnDirectedGraph.java
+++ src/com/matthewddiaz/datastructures/graphs/UnDirectedGraph.java
@@ -9,15 +9,35 @@ import java.util.Set;
  */
 public class UnDirectedGraph extends Graph{
 
+    /**
+     * Constructor for Undirected graph
+     * @param vertexSet set of vertices of the graph
+     * @param edgeSet set of edges of the graph
+     */
     public UnDirectedGraph(Set<Vertex> vertexSet, Set<Edge> edgeSet){
         super(vertexSet, edgeSet);
     }
 
+    /**
+     *
+     * @param source the first vertex in the pair
+     * @param destination the second vertex in the pair
+     * @return returns true if source vertex and destination vertex are not the same.
+     * Note: self-loops are not allowed in the undirected graph.
+     */
     @Override
     boolean isEdgeValid(Vertex source, Vertex destination) {
         return (source.id != destination.id);
     }
 
+    /**
+     *
+     * @param adjArray adjacency array
+     * @param source source vertex
+     * @param adjacentVertex vertex that is adjacent to source vertex
+     * Note: there exists a symmetric relation with adjacency vertices for an edge in
+     * undirected graphs. That means that if u is adjacent to v; then v is also adjacent to u.
+     */
     @Override
     void addAdjacentVertexToAdjArr(List<Vertex>[] adjArray, Vertex source, Vertex adjacentVertex) {
         if(isEdgeValid(source, adjacentVertex)){
@@ -26,12 +46,24 @@ public class UnDirectedGraph extends Graph{
         }
     }
 
+    /**
+     *
+     * @param adjList adjacency array (map)
+     * @param source  source vertex
+     * @param adjacentVertex vertex that is adjacent to source vertex
+     */
     @Override
     void addAdjacentVertexToAdjMap(Map<Integer, List<Vertex>> adjList, Vertex source, Vertex adjacentVertex) {
         addAdjacentVertex(adjList, source, adjacentVertex);
         addAdjacentVertex(adjList, adjacentVertex, source);
     }
 
+    /**
+     *
+     * @param adjMatrix adjacent matrix 2D array
+     * @param source source vertex
+     * @param adjVertex vertex that is adjacent to source vertex
+     */
     @Override
     void addAdjVertexToAdjMatrix(int[][] adjMatrix, Vertex source, Vertex adjVertex) {
         addVertexToAdjMatrix(adjMatrix, source.id, adjVertex.id);
